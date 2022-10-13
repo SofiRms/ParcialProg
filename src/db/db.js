4b8e919a1a6b1b0dba096f59a0b063aae390444e
SofiRms <sofiamagaliramos@gmail.com>
2022-10-13 14:41:21 -0300

    conexión a BD

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnect = async () => {
     //conexión al mongo atlas
     try{
-        mongoose.connect('mongodb+srv://SofiRms:sofiramos@products.vrhvzlp.mongodb.net/?retryWrites=true&w=majority')
+        mongoose.connect('')
         console.log('Base de datos conectada');
     }catch(error){
         console.log('Error al conectar la base de datos', error.message);
